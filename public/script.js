1f23de7020c72eb7ee18ae1ea5215c9a304463bf
pcbobo1 <98348026+pcbobo1@users.noreply.github.com>
2022-02-03 13:09:22 -0500

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,15 +1,31 @@
-function dropDown() {
-    document.getElementById("myDropdown").classList.toggle("show");
+function dropDownLabs() {
+    document.getElementById("dropDownLabs").classList.toggle("showlabs");
+  }
+function dropDownInfo() {
+  document.getElementById("dropDownInfo").classList.toggle("showinfo");
   }
   
   window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
+    if (!event.target.matches('.dropbtnlabs')) {
+      var dropdowns = document.getElementsByClassName("dropdown-contentlabs");
       var i;
       for (i = 0; i < dropdowns.length; i++) {
         var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
+        if (openDropdown.classList.contains('showlabs')) {
+          openDropdown.classList.remove('showlabs');
+        }
+      }
+    }
+  }
+
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtninfo')) {
+      var dropdowns = document.getElementsByClassName("dropdown-contentinfo");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('showinfo')) {
+          openDropdown.classList.remove('showinfo');
         }
       }
     }
