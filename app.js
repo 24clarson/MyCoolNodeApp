42f2e1cde287d87ee85e5f36309780f319a1eb68
24clarson <93607328+24clarson@users.noreply.github.com>
2022-02-10 12:47:28 -0500

    Update app.js

--- app.js
+++ app.js
@@ -26,38 +26,41 @@ app.get('*', (req, res) => {
 app.listen(port,() => {
     console.log(`Server running at port `+port);
 });
+const http = require('http');
+const port = process.env.PORT || 1234
+const express = require('express');
+var bodyParser = require('body-parser')
+const app = express();
+const path = require('path');
+const lessons = require("./lessons.js")
+app.use(express.json());
+app.use(express.static( path.join(__dirname, 'public')))
 
-var gmail = "24me@cpsd.us"
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'))
 
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb+srv://mango:MangoesAreGood@Autograder.mongodb.net/scores?retryWrites=true&w=majority";
+app.get('/header.html', (req, res) => {
+    res.render('lessonTemplate.ejs', lessons.header)
+})
+
+app.get('/paragraph.html', (req, res) => {
+    res.render('lessonTemplate.ejs', lessons.paragraph)
+})
+
+app.get('*', (req, res) => {
+    res.render('sign_in.ejs')
+})
+
+app.listen(port,() => {
+    console.log(`Server running at port `+port);
+});
 
 
-app.post('/score', (request, response) => {
-    console.log(request.body);
-    MongoClient.connect(url, function(err, db) {
-        if (err) throw err;
-        var dbo = db.db("mydb");
-        dbo.collection("scores").find({email: gmail}).toArray(function(err, res) {
-            var userExists = false
-            if (res.length > 0) {
-                userExists = true;
-            }
-            if (userExists) {
-                var newValues = { $set: { score: request.body } };
-                dbo.collection("scores").updateOne({email: gmail}, newValues, function(err, res) {
-                    if (err) throw err;
-                    db.close();
-                });
-            }
-            if (!userExists) {
-                var myobj = {email: gmail, score: request.body};
-                dbo.collection("scores").insertOne(myobj, function(err, res) {
-                    if (err) throw err;
-                    db.close();
-                });
-            }
-        });    
-    });
-    response.end();
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://mango:MangoesAreGood>@scores.nyroz.mongodb.net/mydb?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("mydb").collection("mycol");
+  collection.insertOne({name: 'hello'})
+  client.close();
 });
