6d060d9dd6ff2664a292342210c0a8fd4aa96042
24clarson <93607328+24clarson@users.noreply.github.com>
2022-02-10 13:02:19 -0500

    Update app.js

--- app.js
+++ app.js
@@ -29,46 +29,13 @@ app.listen(port,() => {
 
 
 const { MongoClient } = require('mongodb');
-const uri = "mongodb+srv://mango:MangoesAreGood>@scores.nyroz.mongodb.net/scores?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(async err => {
-  const collection = await client.db("mydb").collection("mycol");
-  console.log("connected!")
-  // perform actions on the collection object
-  const doc = { name: "Neapolitan pizza", shape: "round" };
-  const result = await collection.insertOne(doc);
-  console.log("document inserted")
-  client.close();
+const uri = "mongodb+srv://mango:MangoesAreGood>@scores.nyroz.mongodb.net/mydb?retryWrites=true&w=majority";
+
+app.post('/score', (request, response) => {
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    client.connect(async err => {
+      const collection = await client.db("mydb").collection("mycol");
+      const doc = { name: "Neapolitan pizza", shape: "round" };
+      const result = await collection.insertOne(doc);
+    });
 });
-
-
-// var url = "mongodb://localhost:27017/";
-// var gmail = "24me@cpsd.us"
-// app.post('/score', (request, response) => {
-//     console.log(request.body.test);
-//     MongoClient.connect(url, function(err, db) {
-//         if (err) throw err;
-//         var dbo = db.db("mydb");
-//         dbo.collection("scores").find({email: gmail}).toArray(function(err, res) {
-//             var userExists = false
-//             if (res.length > 0) {
-//                 userExists = true;
-//             }
-//             if (userExists) {
-//                 var newValues = { $set: { score: request.body.test } };
-//                 dbo.collection("scores").updateOne({email: gmail}, newValues, function(err, res) {
-//                     if (err) throw err;
-//                     db.close();
-//                 });
-//             }
-//             if (!userExists) {
-//                 var myobj = {email: gmail, score: request.body.test};
-//                 dbo.collection("scores").insertOne(myobj, function(err, res) {
-//                     if (err) throw err;
-//                     db.close();
-//                 });
-//             }
-//         });    
-//     });
-//     response.end();
-// });
